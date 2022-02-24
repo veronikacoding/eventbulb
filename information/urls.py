f21ddf824a08c49b6ab17fc5ffcb673695e15be3
Veronika Szentesi <szentesiveronika@gmail.com>
2022-02-24 20:57:28 +0000

    meetup page commit

--- information/urls.py
+++ information/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name="information_home")
+    path('', views.home, name="information_home"),
+    path('django_meetup/', views.django_meetup, name="information_django_meetup")
 ]
--- information/views.py
+++ information/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'information/home.html')
+
+def django_meetup(request):
+    return render(request, 'information/django_meetup.html')
